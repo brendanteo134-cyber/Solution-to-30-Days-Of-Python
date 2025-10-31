e1a9c5a00051b0297957e1205556da3ced19f207
brendanteo134-cyber <brendanteo134@gmail.com>
2025-10-31 17:34:46 +0800

    Fix formatting and add app.run for Flask

--- day_29/app.py
+++ day_29/app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 #
 MONGODB_URI = 'mongodb+srv://asabeneh:your_password@30daysofpython-twxkr.mongodb.net/test?retryWrites=true&w=majority'
 client = pymongo.MongoClient(MONGODB_URI)
-db = client['thirty_days_of_python']  # accessing the database
+db = client['thirty_days_of_python']
 
 
 @app.route('/api/v1.0/students', methods=['GET'])
@@ -88,4 +88,5 @@ if __name__ == '__main__':
     # for deployment
     # to make it work for both production and development
     port = int(os.environ.get("PORT", 5000))
+
     app.run(debug=True, host='0.0.0.0', port=port)
